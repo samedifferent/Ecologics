d9854ac2655612fb136dd8c106668223b84430db
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-01-16 17:12:44 -0500

    Add strippables

--- src/main/java/samebutdifferent/ecologics/event/ModEventHandler.java
+++ src/main/java/samebutdifferent/ecologics/event/ModEventHandler.java
@@ -1,10 +1,13 @@
 package samebutdifferent.ecologics.event;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.FoliageColor;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.event.ColorHandlerEvent;
@@ -23,6 +26,9 @@ public class ModEventHandler {
         event.enqueueWork(() -> {
             ModTrunkPlacerTypes.register();
             ModConfiguredFeatures.register();
+            AxeItem.STRIPPABLES = new ImmutableMap.Builder<Block, Block>().putAll(AxeItem.STRIPPABLES)
+                    .put(ModBlocks.COCONUT_LOG.get(), ModBlocks.STRIPPED_COCONUT_LOG.get())
+                    .put(ModBlocks.COCONUT_WOOD.get(), ModBlocks.STRIPPED_COCONUT_WOOD.get()).build();
         });
     }
 
--- src/main/java/samebutdifferent/ecologics/registry/ModConfiguredFeatures.java
+++ src/main/java/samebutdifferent/ecologics/registry/ModConfiguredFeatures.java
@@ -16,12 +16,7 @@ import samebutdifferent.ecologics.worldgen.feature.foliageplacers.CoconutFoliage
 import samebutdifferent.ecologics.worldgen.feature.trunkplacers.SlantedTrunkPlacer;
 
 public class ModConfiguredFeatures {
-    public static final ConfiguredFeature<TreeConfiguration, ?> COCONUT = Feature.TREE.configured(new TreeConfiguration.TreeConfigurationBuilder(
-            BlockStateProvider.simple(ModBlocks.COCONUT_LOG.get()),
-            new SlantedTrunkPlacer(7, 4, 0),
-            BlockStateProvider.simple(ModBlocks.COCONUT_LEAVES.get()),
-            new CoconutFoliagePlacer(ConstantInt.of(0), ConstantInt.of(0)),
-            new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
+    public static final ConfiguredFeature<TreeConfiguration, ?> COCONUT = Feature.TREE.configured(new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(ModBlocks.COCONUT_LOG.get()), new SlantedTrunkPlacer(7, 4, 0), BlockStateProvider.simple(ModBlocks.COCONUT_LEAVES.get()), new CoconutFoliagePlacer(ConstantInt.of(0), ConstantInt.of(0)), new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
 
     public static <FC extends FeatureConfiguration> ConfiguredFeature<FC, ?> register(String pKey, ConfiguredFeature<FC, ?> pConfiguredFeature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(Ecologics.MOD_ID, pKey), pConfiguredFeature);
