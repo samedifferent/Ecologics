2d2faf82354e8df45e79a778982977414174888c
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-01-17 05:18:00 -0500

    Tweak crab ai

--- src/main/java/samebutdifferent/ecologics/entity/CoconutCrab.java
+++ src/main/java/samebutdifferent/ecologics/entity/CoconutCrab.java
@@ -1,23 +1,18 @@
 package samebutdifferent.ecologics.entity;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.util.TimeUtil;
-import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.NeutralMob;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -34,14 +29,9 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-import java.util.UUID;
-
-public class CoconutCrab extends Animal implements NeutralMob, IAnimatable {
+public class CoconutCrab extends Animal implements IAnimatable {
+    private static final EntityDataAccessor<Boolean> HAS_COCONUT = SynchedEntityData.defineId(CoconutCrab.class, EntityDataSerializers.BOOLEAN);
     private final AnimationFactory factory = new AnimationFactory(this);
-    private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);
-    private int remainingPersistentAngerTime;
-    @Nullable
-    private UUID persistentAngerTarget;
 
     public CoconutCrab(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
@@ -64,16 +54,30 @@ public class CoconutCrab extends Animal implements NeutralMob, IAnimatable {
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.goalSelector.addGoal(1, new LeapAtTargetGoal(this, 0.3F));
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, true));
-        this.goalSelector.addGoal(3, new RandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class, 6.0F));
-        this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new HurtByTargetGoal(this).setAlertOthers());
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true, false));
-        this.targetSelector.addGoal(3, new ResetUniversalAngerTargetGoal<>(this, false));
+        this.goalSelector.addGoal(3, new CoconutCrab.CrabAvoidGoal<>(this, Player.class, 8.0F, 2.0D, 2.0D));
+        this.goalSelector.addGoal(4, new RandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+        this.targetSelector.addGoal(1, new CoconutCrab.CrabNearestAttackableTargetGoal<>(this, Player.class, true, false));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 15.0D).add(Attributes.FOLLOW_RANGE, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.25D).add(Attributes.ATTACK_DAMAGE, 2.0D);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 30.0D).add(Attributes.FOLLOW_RANGE, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.25D).add(Attributes.ATTACK_DAMAGE, 2.0D);
+    }
+
+    @Override
+    public boolean hurt(DamageSource pSource, float pAmount) {
+        return super.hurt(pSource, this.hasCoconut() ? pAmount / 2 : pAmount);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (this.getHealth() <= this.getMaxHealth() / 2 && this.hasCoconut()) {
+            this.setHasCoconut(false);
+            this.setTarget(null);
+            this.playCoconutSmashSound();
+        }
     }
 
     @Override
@@ -96,58 +100,42 @@ public class CoconutCrab extends Animal implements NeutralMob, IAnimatable {
         return false;
     }
 
-    @Override
-    public void readAdditionalSaveData(CompoundTag pCompound) {
-        super.readAdditionalSaveData(pCompound);
-        this.readPersistentAngerSaveData(this.level, pCompound);
+    public void setHasCoconut(boolean hasCoconut) {
+        this.entityData.set(HAS_COCONUT, hasCoconut);
+    }
+
+    public boolean hasCoconut() {
+        return this.entityData.get(HAS_COCONUT);
     }
 
     @Override
-    public void addAdditionalSaveData(CompoundTag pCompound) {
-        super.addAdditionalSaveData(pCompound);
-        this.addPersistentAngerSaveData(pCompound);
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(HAS_COCONUT, true);
     }
 
     @Override
-    public int getRemainingPersistentAngerTime() {
-        return this.remainingPersistentAngerTime;
-    }
-
-    @Override
-    public void setRemainingPersistentAngerTime(int pTime) {
-        this.remainingPersistentAngerTime = pTime;
-    }
-
-    @Nullable
-    @Override
-    public UUID getPersistentAngerTarget() {
-        return this.persistentAngerTarget;
-    }
-
-    @Override
-    public void setPersistentAngerTarget(@Nullable UUID pTarget) {
-        this.persistentAngerTarget = pTarget;
-    }
-
-    @Override
-    public void startPersistentAngerTimer() {
-        this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
-    }
-
     protected SoundEvent getAmbientSound() {
         return ModSoundEvents.COCONUT_CRAB_AMBIENT.get();
     }
 
+    @Override
     protected SoundEvent getHurtSound(DamageSource pDamageSource) {
         return ModSoundEvents.COCONUT_CRAB_HURT.get();
     }
 
+    @Override
     protected SoundEvent getDeathSound() {
         return ModSoundEvents.COCONUT_CRAB_DEATH.get();
     }
 
+    @Override
     protected void playStepSound(BlockPos pPos, BlockState pBlock) {
         this.playSound(SoundEvents.SPIDER_STEP, 0.15F, 1.0F);
+    }
+
+    protected void playCoconutSmashSound() {
+        this.playSound(ModSoundEvents.COCONUT_SMASH.get(), 1.0F, 1.0F);
     }
 
     private static  <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
@@ -166,5 +154,33 @@ public class CoconutCrab extends Animal implements NeutralMob, IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return factory;
+    }
+
+    static class CrabAvoidGoal<T extends LivingEntity> extends AvoidEntityGoal<T> {
+        private final CoconutCrab crab;
+
+        public CrabAvoidGoal(CoconutCrab pMob, Class<T> pEntityClassToAvoid, float pMaxDistance, double pWalkSpeedModifier, double pSprintSpeedModifier) {
+            super(pMob, pEntityClassToAvoid, pMaxDistance, pWalkSpeedModifier, pSprintSpeedModifier, EntitySelector.NO_SPECTATORS::test);
+            this.crab = pMob;
+        }
+
+        @Override
+        public boolean canUse() {
+            return !this.crab.hasCoconut() && super.canUse();
+        }
+    }
+
+    static class CrabNearestAttackableTargetGoal<T extends LivingEntity> extends NearestAttackableTargetGoal<T> {
+        private final CoconutCrab crab;
+
+        public CrabNearestAttackableTargetGoal(CoconutCrab pMob, Class<T> pTargetType, boolean pMustSee, boolean pMustReach) {
+            super(pMob, pTargetType, pMustSee, pMustReach);
+            this.crab = pMob;
+        }
+
+        @Override
+        public boolean canUse() {
+            return this.crab.hasCoconut() && super.canUse();
+        }
     }
 }
