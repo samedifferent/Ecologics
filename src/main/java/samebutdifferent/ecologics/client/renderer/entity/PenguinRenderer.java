2a63c3cda90c41b331bb5889cd4be8264d5fe9d4
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-01-30 15:34:22 -0500

    Fix penguin renderer

--- src/main/java/samebutdifferent/ecologics/client/renderer/entity/PenguinRenderer.java
+++ src/main/java/samebutdifferent/ecologics/client/renderer/entity/PenguinRenderer.java
@@ -1,9 +1,6 @@
 package samebutdifferent.ecologics.client.renderer.entity;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import samebutdifferent.ecologics.client.model.PenguinModel;
@@ -16,13 +13,5 @@ public class PenguinRenderer extends GeoEntityRenderer<Penguin> {
     public PenguinRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new PenguinModel());
         this.shadowRadius = 0.4F;
-    }
-
-    @Override
-    public void render(Penguin entity, float entityYaw, float partialTicks, PoseStack stack, MultiBufferSource bufferIn, int packedLightIn) {
-        if (entity.isOnGround() && entity.getFeetBlockState().is(Blocks.SNOW)) {
-            stack.translate(0.0F, 0.126F, 0.0F);
-        }
-        super.render(entity, entityYaw, partialTicks, stack, bufferIn, packedLightIn);
     }
 }
