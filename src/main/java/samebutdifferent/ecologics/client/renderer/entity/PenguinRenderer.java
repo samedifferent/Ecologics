16cdbb118567667c8faf5376a2f525809cd6eb13
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-03-03 14:42:34 -0500

    Cod item renderer working

--- src/main/java/samebutdifferent/ecologics/client/renderer/entity/PenguinRenderer.java
+++ src/main/java/samebutdifferent/ecologics/client/renderer/entity/PenguinRenderer.java
@@ -1,11 +1,17 @@
 package samebutdifferent.ecologics.client.renderer.entity;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import samebutdifferent.ecologics.client.model.PenguinModel;
-import samebutdifferent.ecologics.client.renderer.entity.layers.PenguinCodItemLayer;
 import samebutdifferent.ecologics.entity.Penguin;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 @OnlyIn(Dist.CLIENT)
@@ -14,6 +20,21 @@ public class PenguinRenderer extends GeoEntityRenderer<Penguin> {
     public PenguinRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new PenguinModel());
         this.shadowRadius = 0.4F;
-        this.addLayer(new PenguinCodItemLayer(this));
+    }
+
+    @Override
+    public void renderRecursively(GeoBone bone, PoseStack stack, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+        if (bone.getName().equals("beak")) {
+            stack.pushPose();
+            stack.mulPose(Vector3f.XP.rotationDegrees(-90));
+            stack.mulPose(Vector3f.YP.rotationDegrees(0));
+            stack.mulPose(Vector3f.ZP.rotationDegrees(135));
+            stack.translate(0.25D, -0.25D, 0.6D);
+            stack.scale(0.8F,0.8F,0.8F);
+            Minecraft.getInstance().getItemRenderer().renderStatic(mainHand, ItemTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, packedLightIn, packedOverlayIn, stack, this.rtb, 0);
+            stack.popPose();
+            bufferIn = rtb.getBuffer(RenderType.entityTranslucent(whTexture));
+        }
+        super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
     }
 }
