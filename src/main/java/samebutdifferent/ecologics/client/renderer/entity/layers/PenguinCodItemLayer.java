16ab693047cddd4348f4a5003a4489a0694cb655
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-03-03 12:15:48 -0500

    Tweak cod item renderer

--- src/main/java/samebutdifferent/ecologics/client/renderer/entity/layers/PenguinCodItemLayer.java
+++ src/main/java/samebutdifferent/ecologics/client/renderer/entity/layers/PenguinCodItemLayer.java
@@ -10,7 +10,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import samebutdifferent.ecologics.entity.Penguin;
-import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
@@ -22,12 +21,10 @@ public class PenguinCodItemLayer extends GeoLayerRenderer<Penguin> {
 
     @Override
     public void render(PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight, Penguin pLivingEntity, float pLimbSwing, float pLimbSwingAmount, float pPartialTicks, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
-        GeoBone head = this.getEntityModel().getModel(this.getEntityModel().getModelLocation(pLivingEntity)).getBone("head").get();
         pMatrixStack.pushPose();
-        pMatrixStack.translate((head.getPositionX() / 16.0F), (head.getPositionY() / 16.0F), (head.getPositionZ() / 16.0F));
         pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(pNetHeadYaw));
         pMatrixStack.mulPose(Vector3f.XP.rotationDegrees(pHeadPitch));
-        pMatrixStack.translate(0.0F, 0.6F, -0.1D);
+        pMatrixStack.translate(0.0F, 0.6F, -0.35D);
         pMatrixStack.mulPose(Vector3f.XP.rotationDegrees(90.0F));
         ItemStack stack = pLivingEntity.getItemBySlot(EquipmentSlot.MAINHAND);
         Minecraft.getInstance().getItemInHandRenderer().renderItem(pLivingEntity, stack, ItemTransforms.TransformType.GROUND, false, pMatrixStack, pBuffer, pPackedLight);
