bde49ec117f77f76033ffedd3a46103f9c0c0d96
WspWabbit <abdallmo07@gmail.com>
2022-03-17 16:55:19 -0400

    fixed entity hitboxes -wabbit

--- src/main/java/samebutdifferent/ecologics/registry/ModEntityTypes.java
+++ src/main/java/samebutdifferent/ecologics/registry/ModEntityTypes.java
@@ -11,8 +11,8 @@ import samebutdifferent.ecologics.entity.ModBoat;
 import samebutdifferent.ecologics.entity.Penguin;
 
 public class ModEntityTypes {
-    public static final EntityType<CoconutCrab> COCONUT_CRAB = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "coconut_crab"), EntityType.Builder.create(CoconutCrab::new, SpawnGroup.CREATURE).setDimensions(1.0F, 1.0F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "coconut_crab").toString()));
-    public static final EntityType<Camel> CAMEL = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "camel"), EntityType.Builder.create(Camel::new, SpawnGroup.CREATURE).setDimensions(1.6F, 2.1F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "camel").toString()));
+    public static final EntityType<CoconutCrab> COCONUT_CRAB = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "coconut_crab"), EntityType.Builder.create(CoconutCrab::new, SpawnGroup.CREATURE).setDimensions(0.6F, 0.8F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "coconut_crab").toString()));
+    public static final EntityType<Camel> CAMEL = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "camel"), EntityType.Builder.create(Camel::new, SpawnGroup.CREATURE).setDimensions(1.2F, 2.1F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "camel").toString()));
     public static final EntityType<ModBoat> BOAT = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "boat"), EntityType.Builder.<ModBoat>create(ModBoat::new, SpawnGroup.MISC).setDimensions(1.375F, 0.5625F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "boat").toString()));
-    public static final EntityType<Penguin> PENGUIN = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "penguin"), EntityType.Builder.create(Penguin::new, SpawnGroup.CREATURE).setDimensions(1.0F, 1.0F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "penguin").toString()));
+    public static final EntityType<Penguin> PENGUIN = Registry.register(Registry.ENTITY_TYPE, new Identifier(Ecologics.MOD_ID, "penguin"), EntityType.Builder.create(Penguin::new, SpawnGroup.CREATURE).setDimensions(0.5F, 0.6F).maxTrackingRange(10).build(new Identifier(Ecologics.MOD_ID, "penguin").toString()));
 }
