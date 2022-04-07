3bfea00029333cc790f86af0a29eb6713c9269b2
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2022-04-07 17:17:00 -0400

    Mixin sign failure fix

--- src/main/java/samebutdifferent/ecologics/block/properties/ModWoodType.java
+++ src/main/java/samebutdifferent/ecologics/block/properties/ModWoodType.java
@@ -1,11 +1,10 @@
 package samebutdifferent.ecologics.block.properties;
 
 import net.minecraft.util.SignType;
-import samebutdifferent.ecologics.mixin.MixinSignType;
 
 public class ModWoodType {
-    public static final SignType COCONUT = MixinSignType.registerNew(MixinSignType.newSignType("coconut"));
-    public static final SignType WALNUT = MixinSignType.registerNew(MixinSignType.newSignType("walnut"));
-    public static final SignType AZALEA = MixinSignType.registerNew(MixinSignType.newSignType("azalea"));
-    public static final SignType FLOWERING_AZALEA = MixinSignType.registerNew(MixinSignType.newSignType("flowering_azalea"));
+    public static final SignType COCONUT = SignType.register(new SignType("coconut"));
+    public static final SignType WALNUT = SignType.register(new SignType("walnut"));
+    public static final SignType AZALEA = SignType.register(new SignType("azalea"));
+    public static final SignType FLOWERING_AZALEA = SignType.register(new SignType("flowering_azalea"));
 }
