cc480bdabddfa1674a92998c9cc10550a0334092
onVoid <onvoidmc@gmail.com>
2022-04-12 11:36:21 -0700

    Initial Macaws Bridges compat - Only Azalea and Flowering Azalea so far

---
+++ src/main/java/samebutdifferent/ecologics/compat/mcwbridges/MBCompat.java
@@ -0,0 +1,68 @@
+package samebutdifferent.ecologics.compat.mcwbridges;
+
+import com.mcwbridges.kikoz.MacawsBridges;
+import com.mcwbridges.kikoz.objects.*;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.registries.RegistryObject;
+import samebutdifferent.ecologics.registry.ModBlocks;
+import samebutdifferent.ecologics.registry.ModItems;
+import vectorwing.farmersdelight.FarmersDelight;
+import vectorwing.farmersdelight.common.block.CabinetBlock;
+
+import java.util.function.Supplier;
+
+public class MBCompat {
+    public static final RegistryObject<Block> AZALEA_LOG_BRIDGE_MIDDLE;
+    public static final RegistryObject<Block> FLOWERING_AZALEA_LOG_BRIDGE_MIDDLE;
+
+    public static final RegistryObject<Block> AZALEA_LOG_ROPE_BRIDGE_MIDDLE;
+    public static final RegistryObject<Block> FLOWERING_AZALEA_LOG_ROPE_BRIDGE_MIDDLE;
+
+    public static final RegistryObject<Block> AZALEA_BRIDGE_PIER;
+    public static final RegistryObject<Block> FLOWERING_AZALEA_BRIDGE_PIER;
+
+    public static final RegistryObject<Block> AZALEA_LOG_BRIDGE_STAIR;
+    public static final RegistryObject<Block> FLOWERING_AZALEA_LOG_BRIDGE_STAIR;
+
+    public static final RegistryObject<Block> AZALEA_RAIL_BRIDGE;
+    public static final RegistryObject<Block> FLOWERING_AZALEA_RAIL_BRIDGE;
+
+    public static final RegistryObject<Block> AZALEA_ROPE_BRIDGE_STAIR;
+    public static final RegistryObject<Block> FLOWERING_AZALEA_ROPE_BRIDGE_STAIR;
+
+    static {
+        AZALEA_LOG_BRIDGE_MIDDLE = registerBlock("azalea_log_bridge_middle", () -> new Log_Bridge(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+        FLOWERING_AZALEA_LOG_BRIDGE_MIDDLE = registerBlock("flowering_azalea_log_bridge_middle", () -> new Log_Bridge(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+
+        AZALEA_LOG_ROPE_BRIDGE_MIDDLE = registerBlock("rope_azalea_bridge", () -> new Rope_Bridge(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+        FLOWERING_AZALEA_LOG_ROPE_BRIDGE_MIDDLE = registerBlock("rope_flowering_azalea_bridge", () -> new Rope_Bridge(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+
+        AZALEA_BRIDGE_PIER = registerBlock("azalea_bridge_pier", () -> new Support_Pillar(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+        FLOWERING_AZALEA_BRIDGE_PIER = registerBlock("flowering_azalea_bridge_pier", () -> new Support_Pillar(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+
+        AZALEA_LOG_BRIDGE_STAIR = registerBlock("azalea_log_bridge_stair", () -> new Iron_Stair(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+        FLOWERING_AZALEA_LOG_BRIDGE_STAIR = registerBlock("flowering_azalea_log_bridge_stair", () -> new Iron_Stair(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+
+        AZALEA_RAIL_BRIDGE = registerBlock("azalea_rail_bridge", () -> new Rail_Bridge(BlockBehaviour.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+        FLOWERING_AZALEA_RAIL_BRIDGE = registerBlock("flowering_azalea_rail_bridge", () -> new Rail_Bridge(BlockBehaviour.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+
+        AZALEA_ROPE_BRIDGE_STAIR = registerBlock("azalea_rope_bridge_stair", () -> new Iron_Stair(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(0.8F, 2.0F).sound(SoundType.WOOD)));
+        FLOWERING_AZALEA_ROPE_BRIDGE_STAIR = registerBlock("flowering_azalea_rope_bridge_stair", () -> new Iron_Stair(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(0.8F, 2.0F).sound(SoundType.WOOD)));
+    }
+
+    public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = ModBlocks.BLOCKS.register(name, block);
+        ModItems.ITEMS.register(name, () -> new BlockItem(toReturn.get(), new Item.Properties().tab(MacawsBridges.BridgesItemGroup)));
+        return toReturn;
+    }
+
+    public static void init() {
+    }
+}
---
+++ src/main/java/samebutdifferent/ecologics/compat/mcwbridges/MBCompatClient.java
@@ -0,0 +1,18 @@
+package samebutdifferent.ecologics.compat.mcwbridges;
+
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class MBCompatClient {
+
+    @OnlyIn(Dist.CLIENT)
+    public static void registerRenderLayers(FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(MBCompat.AZALEA_LOG_ROPE_BRIDGE_MIDDLE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(MBCompat.FLOWERING_AZALEA_LOG_ROPE_BRIDGE_MIDDLE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(MBCompat.AZALEA_ROPE_BRIDGE_STAIR.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(MBCompat.FLOWERING_AZALEA_ROPE_BRIDGE_STAIR.get(), RenderType.cutout());
+    }
+}
