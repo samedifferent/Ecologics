b1d953557b974a30b4f36ed25bca57d6320c57e8
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-05-14 21:03:44 -0400

    Make coconut leaves extend LeavesBlock for compat

--- src/main/java/samebutdifferent/ecologics/block/CoconutLeavesBlock.java
+++ src/main/java/samebutdifferent/ecologics/block/CoconutLeavesBlock.java
@@ -2,47 +2,34 @@ package samebutdifferent.ecologics.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.common.IForgeShearable;
 
 import java.util.Random;
 
-public class CoconutLeavesBlock extends Block implements IForgeShearable {
-    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
-    public static final IntegerProperty DISTANCE = IntegerProperty.create("distance", 1, 9);
+public class CoconutLeavesBlock extends LeavesBlock {
+    public static final IntegerProperty DISTANCE_9 = IntegerProperty.create("distance_9", 1, 9);
 
     public CoconutLeavesBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(DISTANCE, 9).setValue(PERSISTENT, false));
-    }
-
-    @Override
-    public VoxelShape getBlockSupportShape(BlockState pState, BlockGetter pReader, BlockPos pPos) {
-        return Shapes.empty();
+        this.registerDefaultState(this.stateDefinition.any().setValue(DISTANCE_9, 9).setValue(PERSISTENT, false).setValue(DISTANCE, 7));
     }
 
     @Override
     public boolean isRandomlyTicking(BlockState pState) {
-        return pState.getValue(DISTANCE) == 9 && !pState.getValue(PERSISTENT);
+        return pState.getValue(DISTANCE_9) == 9 && !pState.getValue(PERSISTENT);
     }
 
     @Override
     public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, Random pRandom) {
-        if (!pState.getValue(PERSISTENT) && pState.getValue(DISTANCE) == 9) {
+        if (!pState.getValue(PERSISTENT) && pState.getValue(DISTANCE_9) == 9) {
             dropResources(pState, pLevel, pPos);
             pLevel.removeBlock(pPos, false);
         }
@@ -55,14 +42,9 @@ public class CoconutLeavesBlock extends Block implements IForgeShearable {
     }
 
     @Override
-    public int getLightBlock(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
-        return 1;
-    }
-
-    @Override
     public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
         int i = getDistanceAt(pFacingState) + 1;
-        if (i != 1 || pState.getValue(DISTANCE) != i) {
+        if (i != 1 || pState.getValue(DISTANCE_9) != i) {
             pLevel.scheduleTick(pCurrentPos, this, 1);
         }
 
@@ -81,36 +63,20 @@ public class CoconutLeavesBlock extends Block implements IForgeShearable {
             }
         }
 
-        return pState.setValue(DISTANCE, i);
+        return pState.setValue(DISTANCE_9, i);
     }
 
     private static int getDistanceAt(BlockState pNeighbor) {
         if (pNeighbor.is(BlockTags.LOGS)) {
             return 0;
         } else {
-            return pNeighbor.getBlock() instanceof CoconutLeavesBlock ? pNeighbor.getValue(DISTANCE) : 9;
-        }
-    }
-
-    @Override
-    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, Random pRandom) {
-        if (pLevel.isRainingAt(pPos.above())) {
-            if (pRandom.nextInt(15) == 1) {
-                BlockPos blockpos = pPos.below();
-                BlockState blockstate = pLevel.getBlockState(blockpos);
-                if (!blockstate.canOcclude() || !blockstate.isFaceSturdy(pLevel, blockpos, Direction.UP)) {
-                    double d0 = (double)pPos.getX() + pRandom.nextDouble();
-                    double d1 = (double)pPos.getY() - 0.05D;
-                    double d2 = (double)pPos.getZ() + pRandom.nextDouble();
-                    pLevel.addParticle(ParticleTypes.DRIPPING_WATER, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-                }
-            }
+            return pNeighbor.getBlock() instanceof CoconutLeavesBlock ? pNeighbor.getValue(DISTANCE_9) : 9;
         }
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(DISTANCE, PERSISTENT);
+        pBuilder.add(DISTANCE_9, PERSISTENT, DISTANCE);
     }
 
     @Override
