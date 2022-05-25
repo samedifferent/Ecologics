c064a0acfe5d46407afddab792fe67b9149f3086
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-05-24 20:54:07 -0400

    Fix coconut crab render type

--- src/main/java/samebutdifferent/ecologics/client/renderer/entity/CoconutCrabRenderer.java
+++ src/main/java/samebutdifferent/ecologics/client/renderer/entity/CoconutCrabRenderer.java
@@ -1,8 +1,14 @@
 package samebutdifferent.ecologics.client.renderer.entity;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 import samebutdifferent.ecologics.client.model.CoconutCrabModel;
 import samebutdifferent.ecologics.entity.CoconutCrab;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
@@ -13,5 +19,10 @@ public class CoconutCrabRenderer extends GeoEntityRenderer<CoconutCrab> {
     public CoconutCrabRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new CoconutCrabModel());
         this.shadowRadius = 0.6F;
+    }
+
+    @Override
+    public RenderType getRenderType(CoconutCrab animatable, float partialTicks, PoseStack stack, @Nullable MultiBufferSource renderTypeBuffer, @Nullable VertexConsumer vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+        return RenderType.entityCutoutNoCull(textureLocation);
     }
 }
