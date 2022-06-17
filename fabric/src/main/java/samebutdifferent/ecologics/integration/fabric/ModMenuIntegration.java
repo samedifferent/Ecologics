6da50b4fb73ad7ad54bde6134bae5411b70d7a67
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-06-17 00:08:35 -0400

    Add mod menu config integration

---
+++ fabric/src/main/java/samebutdifferent/ecologics/integration/fabric/ModMenuIntegration.java
@@ -0,0 +1,16 @@
+package samebutdifferent.ecologics.integration.fabric;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import samebutdifferent.ecologics.registry.fabric.ModConfigFabric;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfigFabric.class, parent).get();
+    }
+}
