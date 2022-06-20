c03bbdeb7310335cd0b4404e1ac9ab40b90211c6
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-06-20 03:39:50 -0400

    Remove unused geckolib method

--- common/src/main/java/samebutdifferent/ecologics/client/renderer/entity/PenguinRenderer.java
+++ common/src/main/java/samebutdifferent/ecologics/client/renderer/entity/PenguinRenderer.java
@@ -25,18 +25,4 @@ public class PenguinRenderer extends MobRenderer<Penguin, PenguinModel> {
         }
         return new ResourceLocation(Ecologics.MOD_ID, "textures/entity/penguin.png");
     }
-
-/*    public void renderRecursively(GeoBone bone, PoseStack stack, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-        if (bone.getName().equals("beak")) {
-            stack.pushPose();
-            stack.mulPose(Vector3f.XP.rotationDegrees(-90));
-            stack.mulPose(Vector3f.ZP.rotationDegrees(135));
-            stack.translate(0.25D, -0.25D, 0.6D);
-            stack.scale(0.8F,0.8F,0.8F);
-            Minecraft.getInstance().getItemRenderer().renderStatic(mainHand, ItemTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, packedLightIn, packedOverlayIn, stack, this.rtb, 0);
-            stack.popPose();
-            bufferIn = rtb.getBuffer(RenderType.entityTranslucent(whTexture));
-        }
-        super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-    }*/
 }
