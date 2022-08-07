f47685690d4541672b971d3733af4a02b82d29ca
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-08-07 16:57:08 +0300

    Fixes #80 winter overhaul incompatibility

--- common/src/main/java/samebutdifferent/ecologics/mixin/LivingEntityMixin.java
+++ common/src/main/java/samebutdifferent/ecologics/mixin/LivingEntityMixin.java
@@ -1,14 +1,13 @@
-package samebutdifferent.ecologics.mixin.fabric;
+package samebutdifferent.ecologics.mixin;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import samebutdifferent.ecologics.registry.ModMobEffects;
 
@@ -18,12 +17,12 @@ public abstract class LivingEntityMixin extends Entity {
         super(pEntityType, pLevel);
     }
 
-    @Redirect(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;getFriction()F"))
-    private float onTravel(Block instance) {
+    @ModifyVariable(method = "travel", at = @At(value = "STORE"), ordinal = 0)
+    private float modifyFriction(float f) {
         if (((Object)this) instanceof LivingEntity living && living.hasEffect(ModMobEffects.SLIPPERY.get())) {
             return 0.98F;
         }
-        return instance.getFriction();
+        return f;
     }
 
     @Inject(method = "getBlockSpeedFactor", at = @At("HEAD"), cancellable = true)
