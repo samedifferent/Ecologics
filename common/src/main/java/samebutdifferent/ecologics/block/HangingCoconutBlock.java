cc23e002f397ebe68495dcefda1ce24e5ec13be4
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2025-07-03 21:48:51 -0400

    Fix falling coconut console spam
    No more spam!

--- common/src/main/java/samebutdifferent/ecologics/block/HangingCoconutBlock.java
+++ common/src/main/java/samebutdifferent/ecologics/block/HangingCoconutBlock.java
@@ -67,7 +67,6 @@ public class HangingCoconutBlock extends FallingBlock implements BonemealableBlo
             } else if (pPos.getY() >= pLevel.getMinBuildHeight() && isFree(pLevel.getBlockState(pPos.below()))){
                 FallingBlockEntity fallingblockentity = FallingBlockEntity.fall(pLevel, pPos, pLevel.getBlockState(pPos));
                 this.falling(fallingblockentity);
-                pLevel.addFreshEntity(fallingblockentity);
                 pLevel.removeBlock(pPos, false);
             }
         }
