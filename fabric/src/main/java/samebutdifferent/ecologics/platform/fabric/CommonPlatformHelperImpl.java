3b392bbf2bb18f0e402b7f21289217af10db59f1
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2025-07-10 00:30:25 -0400

    Remove registerSignType from common platform helpers

--- fabric/src/main/java/samebutdifferent/ecologics/platform/fabric/CommonPlatformHelperImpl.java
+++ fabric/src/main/java/samebutdifferent/ecologics/platform/fabric/CommonPlatformHelperImpl.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.minecraft.core.Holder;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.SpawnPlacementType;
@@ -20,7 +19,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraft.world.level.levelgen.Heightmap;
-import samebutdifferent.ecologics.Ecologics;
 import samebutdifferent.ecologics.mixin.fabric.SpawnPlacementsAccessor;
 import samebutdifferent.ecologics.mixin.fabric.WoodTypeAccessor;
 
@@ -101,7 +99,6 @@ public class CommonPlatformHelperImpl {
     }
 
     public static WoodType registerWoodType(WoodType woodType) {
-        Ecologics.registerSignType(woodType, ResourceLocation.tryParse(woodType.name()).getPath());
         return WoodTypeAccessor.invokeRegister(woodType);
     }
 
--- neoforge/src/main/java/samebutdifferent/ecologics/platform/neoforge/CommonPlatformHelperImpl.java
+++ neoforge/src/main/java/samebutdifferent/ecologics/platform/neoforge/CommonPlatformHelperImpl.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import com.google.common.collect.ImmutableMap;
 
 import net.minecraft.core.Holder;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.SpawnPlacementType;
@@ -20,7 +19,6 @@ import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraft.world.level.levelgen.Heightmap;
-import samebutdifferent.ecologics.Ecologics;
 import samebutdifferent.ecologics.mixin.neoforge.AxeItemAccessor;
 
 // @EventBusSubscriber(modid = Ecologics.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
@@ -102,7 +100,6 @@ public class CommonPlatformHelperImpl
     }
     
     public static WoodType registerWoodType(WoodType woodType) {
-        Ecologics.registerSignType(woodType, ResourceLocation.tryParse(woodType.name()).getPath());
         return WoodType.register(woodType);
     }
 
