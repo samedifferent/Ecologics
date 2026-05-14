a2a5ec25fc4d3a09092314b63b0297a1536e3149
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2026-05-14 17:08:43 -0400

    Thin Ice Upgrades

--- common/src/main/java/samebutdifferent/ecologics/block/ThinIceBlock.java
+++ common/src/main/java/samebutdifferent/ecologics/block/ThinIceBlock.java
@@ -1,11 +1,15 @@
 package samebutdifferent.ecologics.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
@@ -17,13 +21,15 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.Fluids;
 import samebutdifferent.ecologics.registry.ModBlocks;
 import samebutdifferent.ecologics.registry.ModEntityTypes;
 import samebutdifferent.ecologics.registry.ModSoundEvents;
+import samebutdifferent.ecologics.registry.ModTags;
 
 public class ThinIceBlock extends IceBlock {
     public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
-
+    
     public ThinIceBlock() {
         super(Properties.of().friction(0.98F).strength(0.5F).sound(SoundType.GLASS).noOcclusion().isValidSpawn((state, blockGetter, pos, entityType) -> entityType.equals(EntityType.POLAR_BEAR) || entityType.equals(ModEntityTypes.PENGUIN.get())));
         this.registerDefaultState(this.stateDefinition.any().setValue(AGE, 0));
@@ -31,7 +37,7 @@ public class ThinIceBlock extends IceBlock {
 
     @Override
     public void fallOn(Level pLevel, BlockState pState, BlockPos pPos, Entity pEntity, float pFallDistance) {
-        if (pEntity instanceof Player player && EnchantmentHelper.getEnchantmentLevel(Enchantments.FALL_PROTECTION, player) == 0) {
+        if (pEntity instanceof LivingEntity entity && entity.getType().is(ModTags.EntityTypeTags.BREAKS_THIN_ICE) && EnchantmentHelper.getEnchantmentLevel(Enchantments.FALL_PROTECTION, entity) == 0 && pFallDistance > 1) {
             pLevel.playSound(null, pPos, ModSoundEvents.THIN_ICE_CRACK.get(), SoundSource.BLOCKS, 0.7F, 0.9F + pLevel.random.nextFloat() * 0.2F);
             replaceIfThinIce(pPos, 3, pLevel);
             replaceIfThinIce(pPos.north(), 2, pLevel);
@@ -54,7 +60,14 @@ public class ThinIceBlock extends IceBlock {
 
     @Override
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
-        if (pEntity instanceof Player && pState.getValue(AGE) > 0) {
+        if (pEntity instanceof LivingEntity entity && entity.getType().is(ModTags.EntityTypeTags.BREAKS_THIN_ICE) && pState.getValue(AGE) > 0) {
+            this.crack(pState, pLevel, pPos);
+        }
+    }
+    
+    @Override
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        if (pEntity instanceof LivingEntity entity && entity.getType().is(ModTags.EntityTypeTags.BREAKS_THIN_ICE)) {
             this.crack(pState, pLevel, pPos);
         }
     }
@@ -70,7 +83,7 @@ public class ThinIceBlock extends IceBlock {
             if (!pLevel.dimensionType().ultraWarm()) {
             	//TODO: Replace this check.
                 //Material material = pLevel.getBlockState(pPos.below()).getMaterial();
-                if (pState.blocksMotion() || pState.liquid()) {
+                if (pState.blocksMotion() || pState.getFluidState().getType() != Fluids.EMPTY) {
                     pLevel.setBlockAndUpdate(pPos, Blocks.WATER.defaultBlockState());
                 }
             }
